e4f8714578c021971ea273dc55575721b10d7f54
Denis Aleksandrovich Bystruev <dbystruev@me.com>
2019-02-04 09:41:09 +0300

    Connected outlets and action and created QuestionAnswerer

---
+++ Question Bot/QuestionAnswerer.swift
@@ -0,0 +1,15 @@
+//
+//  QuestionAnswerer.swift
+//  Question Bot
+//
+//  Created by Denis Bystruev on 04/02/2019.
+//  Copyright © 2019 Denis Bystruev. All rights reserved.
+//
+
+import Foundation
+
+struct QuestionAnswerer {
+    func answer(question: String) -> String {
+        return "?"
+    }
+}
--- Question Bot/ViewController.swift
+++ Question Bot/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var questionTextView: UITextView!
+    
+    let answerer = QuestionAnswerer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        guard let question = questionTextView.text else { return }
+        
+        let answer = answerer.answer(question: question)
+        
+        answerLabel.text = answer
+    }
+    
 }
 
