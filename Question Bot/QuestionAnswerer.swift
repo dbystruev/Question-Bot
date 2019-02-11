22ac4b0378335f46e1121402e34c0afc11590c92
Denis Aleksandrovich Bystruev <dbystruev@me.com>
2019-02-11 09:40:15 +0300

    End of lessons 11.02.2019

--- Question Bot/QuestionAnswerer.swift
+++ Question Bot/QuestionAnswerer.swift
@@ -9,7 +9,22 @@
 import Foundation
 
 struct QuestionAnswerer {
-    func answer(question: String) -> String {
-        return "?"
+    func answer(question: String) -> String? {
+        
+        let components = question.components(separatedBy: [" ", "\n", ".", ",", "\t", "!", "?", ":", "-", "–", "—", ";"])
+        
+        let nonEmptyComponents = components.filter { !$0.isEmpty }
+        
+        let lowercaseComponents = nonEmptyComponents.map { $0.lowercased() }
+        
+        print(lowercaseComponents)
+        
+        if lowercaseComponents.contains("стихи") {
+            return "Я помню чудное мгновенье..."
+        } else if lowercaseComponents.contains("расписание") {
+            return "Понедельник 9:00 – 9:40"
+        } else {
+            return "Не поняла"
+        }
     }
 }
